7e09bbce92be80949432a02012c2a6e53d765bca
kielkow <matheuskiel@fiorifer.com.br>
2021-04-26 22:33:05 -0300

    feat: make Validations with Yup

--- src/app/validators/FoodList.js
+++ src/app/validators/FoodList.js
@@ -10,7 +10,7 @@ export default async (req, res, next) => {
             .min(1)
             .lowercase()
         )
-        .compact(v => typeof v !== 'string' || v === '')
+        .compact(v => typeof v !== 'string')
         .min(1)
         .required(),
     });
